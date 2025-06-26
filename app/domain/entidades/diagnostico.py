b6cd6e30d11e07de950fb7423cafbc61a9173994
Oswaldo <U22209742@utp.edu.pe>
2025-06-25 23:05:24 -0500

    feat:Adicion de campo content a la entidad diagnostico

--- app/domain/entidades/diagnostico.py
+++ app/domain/entidades/diagnostico.py
@@ -7,9 +7,11 @@ from sqlmodel import SQLModel, Field, Relationship
 class Diagnostico(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name:str
+    content:str
     image_url:str
 
 #request
 class DiagnosticoRequest(BaseModel):
     name: str
+    content: str
     image_url: str
